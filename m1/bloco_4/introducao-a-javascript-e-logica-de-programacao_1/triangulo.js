85d58c850c4e1de87838d018e2ec198f42d483ff
Aremtior <marcosleandrofl@gmail.com>
2021-02-24 16:22:10 -0300

    5 - triangulo com function e return.

--- m1/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/triangulo.js
+++ m1/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/triangulo.js
@@ -2,8 +2,14 @@ let a = 20;
 let b = 30;
 let c = 130;
 
-if (a + b + c == 180) {
-  console.log("É um triângulo");
-} else {
-  console.log("Não é um triângulo");
+function anguloTriangulo() {
+  if (a + b + c == 180) {
+    console.log("É um triângulo");
+    return true;
+  } else {
+    console.log("Não é um triângulo");
+    return false;
+  }
 }
+
+anguloTriangulo();
