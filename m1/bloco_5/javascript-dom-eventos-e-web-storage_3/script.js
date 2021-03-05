856483d1cbdfd7cabcc54bb66e2fc2057af38e78
Aremtior <marcosleandrofl@gmail.com>
2021-03-05 20:38:24 -0300

    exercicios 1 e 2 - dias do mes gerados dinamicamente e botao feriados respectivamente.

--- m1/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
+++ m1/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
@@ -14,3 +14,36 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+function createDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const monthDays = document.getElementById('days');
+  for (let i = 0; i < dezDaysList.length; i++) {
+    const day = dezDaysList[i];
+    const monthListItem = document.createElement('li');
+    monthListItem.innerHTML = day;
+    monthListItem.className = 'day';
+
+    if (day == 24 || day == 25 || day == 31) {
+      monthListItem.className += ' holiday';
+    }
+
+    if (day == 4 || day == 11 || day == 18 || day == 25) {
+      monthListItem.className += ' friday';
+    }
+
+    monthDays.appendChild(monthListItem);
+  }
+}
+
+createDays();
+
+function holidays(txtBtn) {
+  btnsCtn = document.querySelector('.buttons-container');
+  btnHolidays = document.createElement('button');
+  btnHolidays.innerHTML = txtBtn;
+  btnHolidays.id = 'btn-holiday';
+
+  btnsCtn.appendChild(btnHolidays);
+}
+
+holidays('Feriados');
