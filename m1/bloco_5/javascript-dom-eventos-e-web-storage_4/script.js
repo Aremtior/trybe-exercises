93e56001d37700d6b50914fe02ea4894494a809d
Aremtior <marcosleandrofl@gmail.com>
2021-03-06 23:40:10 -0300

    function fontSize criada para alterar o tamanho da fonte.

--- m1/bloco_5/javascript-dom-eventos-e-web-storage_4/script.js
+++ m1/bloco_5/javascript-dom-eventos-e-web-storage_4/script.js
@@ -5,8 +5,34 @@ function settings() {
     let li = document.createElement('li');
     let btn = document.createElement('button');
     btn.innerHTML = name;
+    btn.id = name.toLowerCase();
     li.appendChild(btn);
     list.appendChild(li);
   }
 }
-settings()
+settings();
+
+
+
+function fontSize() {
+  let paragraph = document.getElementsByTagName('p');
+  console.log(paragraph);
+  let pFontSize = parseInt(localStorage.getItem('pFontSize'));
+  console.log(pFontSize);
+  if (pFontSize >= 16 && pFontSize < 18) {
+    localStorage.setItem('pFontSize', '18px');
+    console.log(localStorage.getItem('pFontSize'));
+  } else if (pFontSize >= 18 && pFontSize < 24) {
+    localStorage.setItem('pFontSize', '24px');
+    console.log(localStorage.getItem('pFontSize'));
+  } else {
+    localStorage.setItem('pFontSize', '16px');
+  }
+  for (const p of paragraph) {
+    p.style.fontSize = localStorage.getItem('pFontSize');
+    console.log(localStorage.getItem('pFontSize'));
+  }
+}
+
+let btnSize = document.getElementById('font-size');
+btnSize.addEventListener('click', fontSize);
