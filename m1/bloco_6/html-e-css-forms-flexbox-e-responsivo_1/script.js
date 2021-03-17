86b9aed169a55d89e66bafe943b415c279a0af0f
Aremtior <marcosleandrofl@gmail.com>
2021-03-16 23:31:17 -0300

    funçoes de validaçao criadas e botoes funcionando.

--- m1/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ m1/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -36,10 +36,75 @@ function estados() {
   }
 }
 estados();
+
+function dateValid() {
+  const date = document.getElementById('data-inicio').value;
+  // https://stackoverflow.com/questions/15491894/regex-to-validate-date-format-dd-mm-yyyy
+  return date.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/);
+}
+
+function fieldsValid() {
+  // https://stackoverflow.com/questions/8535084/is-there-any-way-to-select-all-input-elements-of-a-form-including-those-outside
+  const form = document.getElementById('form-resume');
+  var allFormControls = form.elements;
+  for (const control of allFormControls) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/elements
+    if (control.nodeName === 'INPUT' || control.nodeName === 'SELECT' || control.nodeName === 'TEXTAREA') {
+      if (control.value === '') {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function validate() {
+  if (fieldsValid()) {
+    if (dateValid()) {
+      return true;
+    } else {
+      alert('Preencha com uma data na forma dd/mm/yyyy');
+    }
+  } else {
+    alert('O preenchimento de todos os campos é obrigatório!')
+  }
+  return false;
+}
+
+function resumeInformation() {
+  const resume = document.createElement('div');
+  resume.id = 'resume';
+  const form = document.getElementById('form-resume');
+  var allFormControls = form.elements;
+  for (const control of allFormControls) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/elements
+    if (control.nodeName === 'INPUT' || control.nodeName === 'SELECT' || control.nodeName === 'TEXTAREA') {
+      const resumeLine = document.createElement('p');
+      resumeLine.innerHTML = control.value;
+      resume.appendChild(resumeLine);
+    }
+  }
+  document.body.appendChild(resume);
+}
+
 function stopDefAction(evt) {
   evt.preventDefault();
+  if (validate()) {
+    resumeInformation();
+  }
+}
+
+function clearAll() {
+  const resume = document.getElementById('resume');
+  if (resume) {
+    resume.remove();
+  }
 }
 
 document.getElementById('send-btn').addEventListener(
   'click', stopDefAction, false
-  );
+);
+
+document.getElementById('reset-btn').addEventListener(
+  'click', clearAll, false
+);
