b7db29302f571e4c610376ef87ed6a1f2aceb447
Aremtior <marcosleandrofl@gmail.com>
2021-04-03 00:25:16 -0300

    exercicio 2 - funçao prizeDraw.js criada.

--- m1/bloco_8/higher-order-functions-do-javascript-es6_1/1-newEmployees.js
+++ m1/bloco_8/higher-order-functions-do-javascript-es6_1/1-newEmployees.js
@@ -1,12 +1,12 @@
-const nameEmail = (name) => ({ name, email: `${name.split(' ').join('_')}@corporation.com` });
+const nameEmail = (name) => ({ name, email: `${name.toLowerCase().split(' ').join('_')}@corporation.com` });
 // console.log(nameEmail('Pedro Guerra'));
 
-const newEmployees = () => {
+const newEmployees = (callback) => {
   const employees = {
-    id1: nameEmail('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
-    id2: nameEmail('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
-    id3: nameEmail('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
   }
   return employees;
 };
-console.log(newEmployees());
+console.log(newEmployees(nameEmail));
