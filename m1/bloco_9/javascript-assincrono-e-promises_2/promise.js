aeb69e3a6b8224320313ab3e626172e4c987cc21
Aremtior <marcosleandrofl@gmail.com>
2021-04-13 15:57:18 -0300

    array de números aleatórios criado e cálculo do quadrado e soma realizados + then + catch.

--- m1/bloco_9/javascript-assincrono-e-promises_2/promise.js
+++ m1/bloco_9/javascript-assincrono-e-promises_2/promise.js
@@ -1,3 +1,17 @@
 const promise = new Promise((resolve, reject) => {
-  
+  const randNumbers = [];
+  for (let index = 0; index < 10; index += 1) {
+    randNumbers.push(Math.ceil(Math.random() * 50));
+  }
+  const squareNumbers = randNumbers.reduce((acc, rn) => {
+    acc.push(rn * rn);
+    return acc;
+  }, []);
+  const sumSquare = squareNumbers.reduce((a, b) => a + b, 0);
+  if (sumSquare < 8000) {
+    return resolve(sumSquare);
+  }
+  return reject(sumSquare);
 })
+.then(sum => console.log('resolvida', sum))
+.catch(sum => console.log('não resolvida', sum));
