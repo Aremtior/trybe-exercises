7a7c0c025396574dc000289523f6d337676b2edf
Aremtior <marcosleandrofl@gmail.com>
2021-04-14 18:29:57 -0300

    codigo refatorado para usar somente async e await.

--- m1/bloco_9/javascript-assincrono-e-promises_2/promiseRefactoring.js
+++ m1/bloco_9/javascript-assincrono-e-promises_2/promiseRefactoring.js
@@ -1,32 +1,36 @@
-const promise = new Promise((resolve, reject) => {
-  const randNumbers = [];
-  for (let index = 0; index < 10; index += 1) {
-    randNumbers.push(Math.ceil(Math.random() * 50));
+const randNumbers = (arr = [], length = 10) => {
+  for (let index = 0; index < length; index += 1) {
+    arr.push(Math.ceil(Math.random() * 50));
   }
-  const squareNumbers = randNumbers.reduce((acc, rn) => {
-    acc.push(rn * rn);
-    return acc;
-  }, []);
-  const sumSquare = squareNumbers.reduce((a, b) => a + b, 0);
+  return arr;
+}
+const squareNumbers = (arr) => arr.reduce((a, b) => {
+  a.push(b * b);
+  return a;
+}, []);
+
+const arrDivide = (sum) => {
+  return [2, 3, 5, 10].map((number) =>
+      Math.round(sum / number))
+      .reduce((a, b) => a + b);
+}
+
+const arrOperations = () => {
+  const sumSquare = squareNumbers(randNumbers()).reduce((a, b) => a + b, 0);
   if (sumSquare < 8000) {
-    return resolve(sumSquare);
+    return (sumSquare);
   }
-  return reject(sumSquare);
-})
-.then(sum => {
-  console.log('resolvida', sum);
-  const arrDivide = [2, 3, 5, 10];
-  for (let index = 0; index < arrDivide.length; index++) {
-    arrDivide[index] = Math.round(sum / arrDivide[index]);
+  throw new Error();
+}
+
+const arrOp = async () => {
+  try {
+    const sum = await arrOperations();
+    console.log('resolvida', sum);
+    const sumDivide = arrDivide(sum);
+    console.log(sumDivide);
+  } catch (error) {
+    console.log('É mais de oito mil! Essa promise deve estar quebrada!');
   }
-  console.log(arrDivide);
-  return arrDivide;
-})
-.then((arrDivide) => {
-  return new Promise((res, rej) => {
-    const sumDivide = arrDivide.reduce((a, b) => a + b);
-    return res(sumDivide);
-  })
-    .then((s) => console.log(s))
-})
-.catch(sum => console.log('É mais de oito mil! Essa promise deve estar quebrada!', sum));
+}
+arrOp();
