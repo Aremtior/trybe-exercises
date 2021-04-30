0ab280141cd44e92f662bfce473395117f28860f
Aremtior <marcosleandrofl@gmail.com>
2021-04-30 00:17:41 -0300

    exercise 3 - create tests with spyOn and mockRestore.

--- m1/bloco_10/testes-automatizados-com-jest_3/exercise3.test.js
+++ m1/bloco_10/testes-automatizados-com-jest_3/exercise3.test.js
@@ -1,17 +1,30 @@
 const randNum = require('./randomNumber');
 
-test('altera a implementação da função', () => {
+test('altera a implementação da função - recebe 3 parâmetros e os multiplica', () => {
   const mockRandom = jest
   .spyOn(randNum, 'retornaNumeroAleatorio')
-  .mockImplementationOnce((a, b) => a / b)
-  .mockReturnValue(11);
+  .mockImplementation((a, b, c) => a * b * c)
 
-  mockRandom(10, 2);
+  mockRandom(10, 2, 5);
   
   expect(mockRandom).toHaveBeenCalled();
   expect(mockRandom).toHaveBeenCalledTimes(1);
-  expect(mockRandom()).toBe(11);
+  expect(mockRandom(10, 2, 5)).toBe(100);
   expect(mockRandom).toHaveBeenCalledTimes(2);
-  expect(mockRandom(2, 3)).toBe(11);
+  expect(mockRandom(2, 3, 6)).toBe(36);
   expect(mockRandom).toHaveBeenCalledTimes(3);
+
+  randNum.retornaNumeroAleatorio.mockRestore();
+  const mockDouble = jest
+  .spyOn(randNum, 'retornaNumeroAleatorio')
+  .mockImplementation((a) => 2 * a);
+
+  mockDouble(4);
+
+  expect(mockDouble).toHaveBeenCalled();
+  expect(mockDouble).toHaveBeenCalledTimes(1);
+  expect(mockDouble(10)).toBe(20);
+  expect(mockDouble).toHaveBeenCalledTimes(2);
+  expect(mockDouble(5)).toBe(10);
+  expect(mockDouble).toHaveBeenCalledTimes(3);
 });
